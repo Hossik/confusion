a022ea558c9535fa090d8dc00b67fa385f1a89d1
Hossik <hossik1983@gmail.com>
2020-06-29 17:00:21 +0300

    site

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -6,7 +6,7 @@ function RenderLeader({leader}){
   return(
       <Media>
         <Media left middle>
-            <Media object src={leader.image} alt={leader.name} />
+          <Media object src={leader.image} alt={leader.name} />
         </Media>
         <Media body className="ml-5">
           <Media heading>{leader.name}</Media>
